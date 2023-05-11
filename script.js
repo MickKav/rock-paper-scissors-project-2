60b04abeef9330d48b32744c3dcd04cb36fd40d0
MickKav <>
2023-05-11 18:39:22 +0000

    Create generateComputerChoice function

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const computerChoice = document.getElementById('computer-choice');
-const userChoice = document.getElementById('your-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3 + 1)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
