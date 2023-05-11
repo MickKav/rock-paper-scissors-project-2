fab18bb616d8d1de135ff98d1b14564ceb731e00
MickKav <>
2023-05-11 19:04:15 +0000

    Define variables for user, computer and result

---
+++ script.js
@@ -0,0 +1,5 @@
+const userChoiceDisplay = document.createElement('h1');
+const computerChoiceDisplay = document.createElement('h1');
+const resultDisplay = document.createElement('h1');
+const gameGrid = document.getElementById('game');
+gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
