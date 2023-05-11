22fc067b116e1aae76e40630ce60b1a11fb9cb9e
MickKav <>
2023-05-11 19:54:29 +0000

    Manipulate computerChoice to act randomly through and integer count of 3

--- script.js
+++ script.js
@@ -9,4 +9,20 @@ let computerChoice
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice()
 }));
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    
+    if (randomNumber === 1) {
+        computerChoice = 'rock';
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors';
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper';
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
