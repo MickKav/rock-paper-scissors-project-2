009e759379802072467165d13df7119775606745
MickKav <>
2023-05-11 18:10:58 +0000

    Get choice and result elements by id in JS

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 const computerChoice = document.getElementById('computer-choice');
+const userChoice = document.getElementById('your-choice');
+const resultDisplay = document.getElementById('result');
