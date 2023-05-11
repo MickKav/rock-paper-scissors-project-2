8c5e65b39f83d404bb94ed7e98f11c9bd3bc5696
MickKav <>
2023-05-11 18:18:20 +0000

    Add event listener to possible choice

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const computerChoice = document.getElementById('computer-choice');
 const userChoice = document.getElementById('your-choice');
 const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
